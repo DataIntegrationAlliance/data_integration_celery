230081a54664e6c342b9cbfc7a7067958bd549e9
mmmaaaggg <mmmaaaggg@163.com>
2018-07-26 19:11:15 +0800

    实现基本功能框架

---
+++ tasks/config.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/3/30 17:53
+@File    : config.py
+@contact : mmmaaaggg@163.com
+@desc    :
+"""
+import os
+import logging
+from logging.config import dictConfig
+import platform
+basedir = os.path.abspath(os.path.dirname(__file__))
+IS_LINUX_OS = platform.os.name != 'nt'
+if IS_LINUX_OS:
+    from celery.schedules import crontab
+
+
+class CeleryConfig:
+    # Celery settings
+    broker_url = 'amqp://mg:***@localhost:5672/celery_tasks',
+    result_backend = 'amqp://mg:***@localhost:5672/backend'
+    accept_content = ['json']  # , 'pickle'
+    timezone = 'Asia/Shanghai'
+    imports = ('tasks', )
+    # beat_schedule = {
+    #     'ptask': {
+    #         'task': 'celery_task.task.chain_task',
+    #         # 'schedule': timedelta(seconds=5),
+    #         'schedule': crontab(hour='18') if IS_LINUX_OS else None,
+    #         # 'args': (16, 73),  # 当前任务没有参数
+    #     },
+    # }
+
+
+# Use a Class-based config to avoid needing a 2nd file
+# os.getenv() enables configuration through OS environment variables
+class ConfigClass(object):
+    # Sql Alchemy settings
+    DB_NAME_MD = 'md'
+    DB_URL_DIC = {
+        DB_NAME_MD: 'mysql://mg:***@10.0.3.66/md_integration?charset=utf8'
+    }
+
+    # ifind settings
+    IFIND_REST_URL = "http://localhost:5000/iFind/"
+
+    # log settings
+    logging_config = dict(
+        version=1,
+        formatters={
+            'simple': {
+                'format': '%(asctime)s %(name)s|%(module)s.%(funcName)s:%(lineno)d %(levelname)s %(message)s'}
+        },
+        handlers={
+            'file_handler':
+                {
+                    'class': 'logging.handlers.RotatingFileHandler',
+                    'filename': 'logger.log',
+                    'maxBytes': 1024 * 1024 * 10,
+                    'backupCount': 5,
+                    'level': 'DEBUG',
+                    'formatter': 'simple',
+                    'encoding': 'utf8'
+                },
+            'console_handler':
+                {
+                    'class': 'logging.StreamHandler',
+                    'level': 'DEBUG',
+                    'formatter': 'simple'
+                }
+        },
+
+        root={
+            'handlers': ['console_handler', 'file_handler'],
+            'level': logging.DEBUG,
+        }
+    )
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.WARN)
+    dictConfig(logging_config)
+
+
+config = ConfigClass()
+celery_config = CeleryConfig()
