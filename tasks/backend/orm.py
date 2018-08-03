6daaa5cb007bb9805213317f6616177ef488c328
mmmaaaggg <mmmaaaggg@163.com>
2018-08-03 18:43:47 +0800

    自动添加table主键

--- tasks/backend/orm.py
+++ tasks/backend/orm.py
@@ -7,43 +7,50 @@
 @contact : mmmaaaggg@163.com
 @desc    :
 """
-from sqlalchemy import Column, Integer, String, UniqueConstraint, TIMESTAMP
+from sqlalchemy import MetaData, Column, Integer, String, UniqueConstraint, TIMESTAMP
 from sqlalchemy.dialects.mysql import DOUBLE
 from sqlalchemy.ext.declarative import declarative_base
 from tasks.utils.db_utils import with_db_session
 from tasks.backend import engine_md
 from tasks.config import config
 import logging
+from tasks.utils.db_utils import alter_table_2_myisam
 logger = logging.getLogger()
-BaseModel = declarative_base()
 
 
 def init(alter_table=False):
-    BaseModel.metadata.create_all(engine_md)
     if alter_table:
-        with with_db_session(engine=engine_md) as session:
-            for table_name, _ in BaseModel.metadata.tables.items():
-                sql_str = "show table status from " + config.DB_SCHEMA_MD + " where name=:table_name"
-                row_data = session.execute(sql_str, params={'table_name': table_name}).first()
-                if row_data is None:
-                    continue
-                if row_data[1].lower() == 'myisam':
-                    continue
+        alter_table_2_myisam(engine_md)
 
-                logger.info('修改 %s 表引擎为 MyISAM', table_name)
-                sql_str = "ALTER TABLE %s ENGINE = MyISAM" % table_name
-                session.execute(sql_str)
+    # 将info、daily表自动增加主键
+    query_pk_str = """SELECT column_name FROM INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` 
+      WHERE table_name='sys_user' AND CONSTRAINT_SCHEMA=:DB_NAME_MD AND constraint_name='PRIMARY'"""
+    create_daily_pk_str = """ALTER TABLE %s
+        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL ,
+        CHANGE COLUMN `time` `time` DATE NOT NULL ,
+        ADD PRIMARY KEY (`ths_code`, `time`)"""
+    create_info_pk_str = """ALTER TABLE %s
+        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL ,
+        ADD PRIMARY KEY (`ths_code`)"""
+    table_name_list = engine_md.table_names()
+    table_count = len(table_name_list)
+    with with_db_session(engine_md) as session:
+        for num, table_name in enumerate(table_name_list, start=1):
+            if table_name.find('_daily') != -1:
+                col_name = session.execute(query_pk_str, params={'schema': config.DB_NAME_MD,
+                                             'table_name': table_name}).scalar()
+                if col_name is None:
+                    # 如果没有记录则 创建主键
+                    session.execute(create_daily_pk_str % table_name)
+                    logger.info('%d/%d) %s 建立主键 (ths_code, time)', num, table_count, table_name)
 
-            sql_str = """select table_name from information_schema.columns 
-              where table_schema = :table_schema and column_name = 'ts_start' and extra <> ''"""
-            table_name_list = [row_data[0]
-                               for row_data in session.execute(sql_str, params={'table_schema': config.DB_SCHEMA_MD})]
-
-            for table_name in table_name_list:
-                logger.info('修改 %s 表 ts_start 默认值，剔除 on update 默认项', table_name)
-                # TimeStamp 类型的数据会被自动设置 default: 'CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'
-                # 需要将 “on update CURRENT_TIMESTAMP”剔除，否则在执行更新时可能会引起错误
-                session.execute("ALTER TABLE " + table_name + " CHANGE COLUMN `ts_start` `ts_start` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
+            elif table_name.find('_info') != -1:
+                col_name = session.execute(query_pk_str, params={'schema': config.DB_NAME_MD,
+                                             'table_name': table_name})
+                if col_name is None:
+                    # 如果没有记录则 创建主键
+                    session.execute(create_info_pk_str % table_name)
+                    logger.info('%d/%d) %s 建立主键 (ths_code)', num, table_count, table_name)
 
     logger.info("所有表结构建立完成")
 
