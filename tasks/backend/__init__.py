98c27467d46b4bc4fc6f957d41af99a0724d458d
mmmaaaggg <mmmaaaggg@163.com>
2018-08-03 18:44:10 +0800

    增加 metadata

--- tasks/backend/__init__.py
+++ tasks/backend/__init__.py
@@ -7,9 +7,10 @@
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from tasks.config import config
 
 engine_dic = {key: create_engine(url) for key, url in config.DB_URL_DIC.items()}
-
+metadata_dic = {key: MetaData(engine) for key, engine in engine_dic.items()}
 engine_md = engine_dic[config.DB_NAME_MD]
+metadata_md = metadata_dic[config.DB_NAME_MD]
