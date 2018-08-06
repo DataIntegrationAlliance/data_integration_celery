7eea5acfd8ec92c11774b72c0e9f2119e776df13
mmmaaaggg <mmmaaaggg@163.com>
2018-08-06 18:24:07 +0800

    自动调整主键位置

--- tasks/backend/orm.py
+++ tasks/backend/orm.py
@@ -39,6 +39,22 @@ def init(alter_table=False):
     if alter_table:
         alter_table_2_myisam(engine_md)
 
+    table_name_list = engine_md.table_names()
+    build_primary_key(table_name_list)
+    logger.info("所有表结构调整完成")
+
+    for table_name in table_name_list:
+        TABLE_MODEL_DIC[table_name] = Table(table_name, Base.metadata, autoload=True)
+
+    logger.info("所有表Model动态加载完成")
+
+
+def build_primary_key(table_name_list):
+    """
+    自动判断表名称，将info、daily表自动增加主键
+    :param table_name_list:
+    :return:
+    """
     # 将info、daily表自动增加主键
     query_pk_str = """SELECT column_name FROM INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` 
         WHERE table_name=:table_name AND CONSTRAINT_SCHEMA=:schema AND constraint_name='PRIMARY'"""
@@ -46,10 +62,14 @@ def init(alter_table=False):
         CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL ,
         CHANGE COLUMN `time` `time` DATE NOT NULL ,
         ADD PRIMARY KEY (`ths_code`, `time`)"""
+    move_daily_col_str = """ALTER TABLE %s
+        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST,
+        CHANGE COLUMN `time` `time` DATE NOT NULL AFTER `ths_code`"""
     create_info_pk_str = """ALTER TABLE %s
         CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL ,
         ADD PRIMARY KEY (`ths_code`)"""
-    table_name_list = engine_md.table_names()
+    move_info_col_str = """ALTER TABLE %s
+        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST"""
     table_count = len(table_name_list)
     with with_db_session(engine_md) as session:
         for num, table_name in enumerate(table_name_list, start=1):
@@ -60,23 +80,20 @@ def init(alter_table=False):
                 if col_name is None:
                     # 如果没有记录则 创建主键
                     session.execute(create_daily_pk_str % table_name)
-                    logger.info('%d/%d) %s 建立主键 (ths_code, time)', num, table_count, table_name)
+                    session.execute(move_daily_col_str % table_name)
+                    logger.info('%d/%d) %s 建立主键 [ths_code, time]', num, table_count, table_name)
 
             elif table_name.find('_info') != -1:
                 col_name = session.execute(query_pk_str,
                                            params={'schema': config.DB_NAME_MD,
-                                                   'table_name': table_name})
+                                                   'table_name': table_name}).scalar()
                 if col_name is None:
                     # 如果没有记录则 创建主键
                     session.execute(create_info_pk_str % table_name)
-                    logger.info('%d/%d) %s 建立主键 (ths_code)', num, table_count, table_name)
-
-    logger.info("所有表结构调整完成")
-
-    for table_name in table_name_list:
-        TABLE_MODEL_DIC[table_name] = Table(table_name, Base.metadata, autoload=True)
-
-    logger.info("所有表Model动态加载完成")
+                    session.execute(move_info_col_str % table_name)
+                    logger.info('%d/%d) %s 建立主键 [ths_code]', num, table_count, table_name)
+            else:
+                logger.debug('%d/%d) %s 无需操作', num, table_count, table_name)
 
 
 if __name__ == "__main__":
