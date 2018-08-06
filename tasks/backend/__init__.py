07828c9002928c73474002b24457369e20709b32
mmmaaaggg <mmmaaaggg@163.com>
2018-08-06 15:13:39 +0800

    增加 code_mapping 自动创建 MyISAM 引擎

--- tasks/backend/__init__.py
+++ tasks/backend/__init__.py
@@ -11,6 +11,4 @@ from sqlalchemy import create_engine, MetaData
 from tasks.config import config
 
 engine_dic = {key: create_engine(url) for key, url in config.DB_URL_DIC.items()}
-metadata_dic = {key: MetaData(engine) for key, engine in engine_dic.items()}
 engine_md = engine_dic[config.DB_NAME_MD]
-metadata_md = metadata_dic[config.DB_NAME_MD]
--- tasks/backend/orm.py
+++ tasks/backend/orm.py
@@ -7,7 +7,7 @@
 @contact : mmmaaaggg@163.com
 @desc    :
 """
-from sqlalchemy import MetaData, Column, Integer, String, UniqueConstraint, TIMESTAMP
+from sqlalchemy import MetaData, Column, Integer, String, UniqueConstraint, TIMESTAMP, Table
 from sqlalchemy.dialects.mysql import DOUBLE
 from sqlalchemy.ext.declarative import declarative_base
 from tasks.utils.db_utils import with_db_session
@@ -16,9 +16,25 @@ from tasks.config import config
 import logging
 from tasks.utils.db_utils import alter_table_2_myisam
 logger = logging.getLogger()
+Base = declarative_base()
+TABLE_MODEL_DIC = {}
+
+
+class CodeMapping(Base):
+    __tablename__ = 'code_mapping'
+    __table_args__ = {'mysql_engine': 'MyISAM'}
+    unique_code = Column(String(20), primary_key=True, comment='统一编码，级别原则是 code.market，例如：600123.SH')
+    wind_code = Column(String(20), comment='万得code')
+    ths_code = Column(String(20), comment='同花顺code')
+    market = Column(String(20), comment='所在市场：SH, SZ, HK, ')
+    type = Column(String(20), comment='资产类型：stock, fund, index, future, option')
 
 
 def init(alter_table=False):
+    # 创建表
+    Base.metadata.create_all(engine_md)
+    logger.info("所有表结构建立完成")
+
     if alter_table:
         alter_table_2_myisam(engine_md)
 
@@ -52,7 +68,12 @@ def init(alter_table=False):
                     session.execute(create_info_pk_str % table_name)
                     logger.info('%d/%d) %s 建立主键 (ths_code)', num, table_count, table_name)
 
-    logger.info("所有表结构建立完成")
+    logger.info("所有表结构调整完成")
+
+    for table_name in table_name_list:
+        TABLE_MODEL_DIC[table_name] = Table(table_name, Base.metadata, autoload=True)
+
+    logger.info("所有表Model动态加载完成")
 
 
 if __name__ == "__main__":
