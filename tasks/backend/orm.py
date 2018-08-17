ef4727a22882561320201fb3a0f58f821156490d
mmmaaaggg <mmmaaaggg@163.com>
2018-08-17 10:34:03 +0800

    支持更新 合并后表 的主键修改

--- tasks/backend/orm.py
+++ tasks/backend/orm.py
@@ -17,7 +17,7 @@ import logging
 from tasks.utils.db_utils import alter_table_2_myisam
 
 logger = logging.getLogger()
-Base = declarative_base()
+Base = declarative_base(bind=engine_md)
 TABLE_MODEL_DIC = {}
 
 
@@ -58,26 +58,18 @@ def build_primary_key(table_name_list):
     # 将info、daily表自动增加主键
     query_pk_str = """SELECT column_name FROM INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` 
         WHERE table_name=:table_name AND CONSTRAINT_SCHEMA=:schema AND constraint_name='PRIMARY'"""
-    create_ifind_daily_pk_str = """ALTER TABLE %s
-        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST,
-        CHANGE COLUMN `time` `time` DATE NOT NULL AFTER `ths_code`,
-        ADD PRIMARY KEY (`ths_code`, `time`)"""
-    create_ifind_info_pk_str = """ALTER TABLE %s
-        CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST,
-        ADD PRIMARY KEY (`ths_code`)"""
-
-    create_wind_daily_pk_str = """ALTER TABLE %s
-        CHANGE COLUMN `wind_code` `wind_code` VARCHAR(20) NOT NULL FIRST,
-        CHANGE COLUMN `trade_date` `trade_date` DATE NOT NULL AFTER `wind_code`,
-        ADD PRIMARY KEY (`wind_code`, `trade_date`)"""
-    create_wind_info_pk_str = """ALTER TABLE %s
-        CHANGE COLUMN `wind_code` `wind_code` VARCHAR(20) NOT NULL FIRST,
-        ADD PRIMARY KEY (`wind_code`)"""
 
     table_count = len(table_name_list)
     with with_db_session(engine_md) as session:
         for num, table_name in enumerate(table_name_list, start=1):
             if table_name.find('ifind_') != -1:
+                create_ifind_daily_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST,
+                    CHANGE COLUMN `time` `time` DATE NOT NULL AFTER `ths_code`,
+                    ADD PRIMARY KEY (`ths_code`, `time`)"""
+                create_ifind_info_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `ths_code` `ths_code` VARCHAR(20) NOT NULL FIRST,
+                    ADD PRIMARY KEY (`ths_code`)"""
                 if table_name.find('_daily') != -1:
                     col_name = session.execute(query_pk_str,
                                                params={'schema': config.DB_SCHEMA_MD,
@@ -98,6 +90,13 @@ def build_primary_key(table_name_list):
                 else:
                     logger.debug('%d/%d) %s 无需操作', num, table_count, table_name)
             elif table_name.find('wind_') != -1:
+                create_wind_daily_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `wind_code` `wind_code` VARCHAR(20) NOT NULL FIRST,
+                    CHANGE COLUMN `trade_date` `trade_date` DATE NOT NULL AFTER `wind_code`,
+                    ADD PRIMARY KEY (`wind_code`, `trade_date`)"""
+                create_wind_info_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `wind_code` `wind_code` VARCHAR(20) NOT NULL FIRST,
+                    ADD PRIMARY KEY (`wind_code`)"""
                 if table_name.find('_daily') != -1:
                     col_name = session.execute(query_pk_str,
                                                params={'schema': config.DB_SCHEMA_MD,
@@ -117,6 +116,34 @@ def build_primary_key(table_name_list):
                         logger.info('%d/%d) %s 建立主键 [wind_code]', num, table_count, table_name)
                 else:
                     logger.debug('%d/%d) %s 无需操作', num, table_count, table_name)
+            else:
+                create_daily_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `unique_code` `unique_code` VARCHAR(20) NOT NULL FIRST,
+                    CHANGE COLUMN `trade_date` `trade_date` DATE NOT NULL AFTER `unique_code`,
+                    ADD PRIMARY KEY (`unique_code`, `trade_date`)"""
+                create_info_pk_str = """ALTER TABLE %s
+                    CHANGE COLUMN `unique_code` `unique_code` VARCHAR(20) NOT NULL FIRST,
+                    ADD PRIMARY KEY (`unique_code`)"""
+                if table_name.find('_daily') != -1:
+                    col_name = session.execute(query_pk_str,
+                                               params={'schema': config.DB_SCHEMA_MD,
+                                                       'table_name': table_name}).scalar()
+                    if col_name is None:
+                        # 如果没有记录则 创建主键
+                        session.execute(create_daily_pk_str % table_name)
+                        logger.info('%d/%d) %s 建立主键 [unique_code, trade_date]', num, table_count, table_name)
+
+                elif table_name.find('_info') != -1:
+                    col_name = session.execute(query_pk_str,
+                                               params={'schema': config.DB_SCHEMA_MD,
+                                                       'table_name': table_name}).scalar()
+                    if col_name is None:
+                        # 如果没有记录则 创建主键
+                        session.execute(create_info_pk_str % table_name)
+                        logger.info('%d/%d) %s 建立主键 [unique_code]', num, table_count, table_name)
+                else:
+                    logger.debug('%d/%d) %s 无需操作', num, table_count, table_name)
+
 
 
 if __name__ == "__main__":
