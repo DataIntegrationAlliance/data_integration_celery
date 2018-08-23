82e63eefa3d29598f1fb67d625dd3aa536173c8f
mmmaaaggg <mmmaaaggg@163.com>
2018-08-23 19:12:07 +0800

    去除分红

--- tasks/wind/__init__.py
+++ tasks/wind/__init__.py
@@ -13,7 +13,7 @@ from direstinvoker.iwind import WindRestInvoker
 invoker = WindRestInvoker(config.WIND_REST_URL)
 
 # 以下语句不能够提前，将会导致循环引用异常
-from tasks.wind.bonus_import import *
+# from tasks.wind.bonus_import import *
 from tasks.wind.commodity import *
 from tasks.wind.convertible_bond import *
 from tasks.wind.future import *
