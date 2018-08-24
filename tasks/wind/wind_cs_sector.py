b836688605ecdb2c83b49a631baa7c73c60479c8
chenfei <18514428716@163.com>
2018-08-24 18:12:27 +0800

    导入文件模块修改成功，内容需修改

--- tasks/wind/wind_cs_sector.py
+++ tasks/wind/wind_cs_sector.py
@@ -5,15 +5,16 @@ Created on Thu Apr 13 16:41:56 2017
 @author: Yupeng Guo
 """
 
-from WindPy import w
-from fh_tools.windy_utils_rest import wind_rest
-import pandas as pd
-from datetime import date, timedelta
-from sqlalchemy.types import String, Date
-from config_fh import get_db_engine, WIND_REST_URL
 import logging
 
-rest = wind_rest(WIND_REST_URL)
+import pandas as pd
+from tasks.wind import invoker
+from datetime import date, timedelta
+from sqlalchemy.types import String, Date
+from tasks.utils.db_utils import with_db_session
+from tasks.backend import engine_md
+from tasks.utils.db_utils import bunch_insert_on_duplicate_update
+
 
 def wind_CS_sector_update():
     dic = {u'CS石油石化':'b101000000000000',
@@ -39,7 +40,7 @@ def wind_CS_sector_update():
             u'CS银行': 'b10l000000000000',
             u'CS证券II': 'b10m010000000000',
             u'CS保险II': 'b10m020000000000',
-            u'CS信托及其他': 'b10m030000000000',    ##### 
+            u'CS信托及其他': 'b10m030000000000',
             u'CS房地产': 'b10n000000000000',
             u'CS交通运输': 'b10o000000000000',
             u'CS电子元器件': 'b10p000000000000',
@@ -47,27 +48,28 @@ def wind_CS_sector_update():
             u'CS计算机': 'b10r000000000000',
             u'CS传媒': 'b10s000000000000',
             u'CS综合': 'b10t000000000000'}
-    w.start()
-    engine = get_db_engine()
-    info = pd.read_sql_query('select sector, max(date) as last_date from wind_CS_sector group by sector', engine)
+    table_name = 'wind_CS_sector'
+    has_table = engine_md.has_table(table_name)
+    param_list = [
+        ('sector', String(20)),
+        ('date',  Date),
+    ]
+    dtype = {col_name: val for col_name, val in param_list}
+    dtype['wind_code'] = String(20)
+    info = pd.read_sql_query('select sector, max(date) as last_date from wind_CS_sector group by sector', engine_md)
     info.set_index('sector', inplace=True)
     for sector in info.index:
         begin_date = info.loc[sector,'last_date'] + timedelta(days=1)
-        week_ends = w.tdays(beginTime=begin_date, endTime=date.today(), options='Period=W').Times ##
+        week_ends = invoker.tdays(beginTime=begin_date, endTime=date.today(), options='Period=W').Times ##
                              
     for week_end in week_ends:
         for sector in dic.keys():              
-            df = rest.wset("sectorconstituent","date=%s;sectorid=%s" %(week_end.date().strftime('%Y-%m-%d'), dic[sector]))
+            df = invoker.wset("sectorconstituent","date=%s;sectorid=%s" %(week_end.date().strftime('%Y-%m-%d'), dic[sector]))
             if len(df) == 0:
                 continue
             df['sector'] = sector
             df.set_index(['sector', 'date', 'wind_code'], inplace=True)
-            df.to_sql('wind_CS_sector', engine, if_exists='append', index_label=['sector','date','wind_code'],
-               dtype={
-                    'sector':String(20),
-                    'date':Date,
-                    'wind_code':String(20)
-                    })
+            bunch_insert_on_duplicate_update(df, table_name, engine_md, dtype=dtype)
             print('Success import %s - %s' %(week_end, sector))
             
-temp = pd.read_sql_table('factors_d', engine)
+temp = pd.read_sql_table('factors_d', engine_md)
