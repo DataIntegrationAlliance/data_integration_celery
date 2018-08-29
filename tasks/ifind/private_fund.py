3281e79595e1b301fee740c3ae59feb1989c3a2f
mmmaaaggg <mmmaaaggg@163.com>
2018-08-29 17:09:38 +0800

    增加导入已清盘数据，代码健壮性增强

--- tasks/ifind/private_fund.py
+++ tasks/ifind/private_fund.py
@@ -2,6 +2,7 @@
 """
 Created on 2018/1/17
 @author: MG
+@desc    : 2018-08-29 已经正式运行测试完成，可以正常使用
 """
 
 import logging
@@ -13,7 +14,7 @@ from tasks.utils.fh_utils import STR_FORMAT_DATE
 from sqlalchemy.types import String, Date, Integer, Boolean
 from sqlalchemy.dialects.mysql import DOUBLE
 from tasks.utils.fh_utils import unzip_join
-from tasks.utils.db_utils import with_db_session, alter_table_2_myisam
+from tasks.utils.db_utils import with_db_session, alter_table_2_myisam, bunch_insert_on_duplicate_update
 from tasks.backend import engine_md
 from tasks import app
 from tasks.merge.code_mapping import update_from_info_table
@@ -26,22 +27,33 @@ ONE_DAY = timedelta(days=1)
 BASE_LINE_HOUR = 20
 
 
-def get_private_fund_set(date_fetch):
+def get_private_fund_set(date_fetch, field='051010001'):
+    """
+    获取基金代码表
+    :param date_fetch:
+    :param field:
+    阳光私募类-全部(已成立、未到期):051010001
+    阳光私募类-已到期(阳光私募):051010005
+    :return:
+    """
     date_fetch_str = date_fetch.strftime(STR_FORMAT_DATE)
+    # TODO: 增加对已到期基金的信息更新
     # 全部私募基金 051010001 仅用测试使用的小数据集（051010020005002）
-    fund_df = invoker.THS_DataPool('block', date_fetch_str + ';051010001', 'date:Y,thscode:Y,security_name:Y')
+    # 阳光私募类-全部(已成立、未到期):051010001
+    # 阳光私募类-已到期(阳光私募):051010005
+    fund_df = invoker.THS_DataPool('block', date_fetch_str + ';' + field, 'date:Y,thscode:Y,security_name:Y')
     if fund_df is None:
         logging.warning('%s 获取基金代码失败', date_fetch_str)
         return None
     fund_count = fund_df.shape[0]
-    logging.info('get %d private fund on %s', fund_count, date_fetch_str)
+    logging.info('get %d private fund on %s of %s', fund_count, date_fetch_str, field)
     return set(fund_df['THSCODE'])
 
 
 @app.task
 def import_fund_info(ths_code=None, refresh=False):
     """
-
+    更新基础信息表
     :param ths_code:
     :param refresh:
     :return:
@@ -51,22 +63,31 @@ def import_fund_info(ths_code=None, refresh=False):
     logging.info("更新 %s 开始", table_name)
     if ths_code is None:
         # 获取全市场私募基金代码及名称
-        if refresh:
-            date_fetch = datetime.strptime('1991-02-01', STR_FORMAT_DATE).date()
-        else:
-            date_fetch = date.today()
 
         date_end = date.today()
         private_fund_set = set()
-        while date_fetch < date_end:
-            private_fund_set_sub = get_private_fund_set(date_fetch)
-            if private_fund_set_sub is not None:
-                private_fund_set |= private_fund_set_sub
-            date_fetch += timedelta(days=90)
 
-        private_fund_set_sub = get_private_fund_set(date_end)
-        if private_fund_set_sub is not None:
-            private_fund_set |= private_fund_set_sub
+        if not refresh and has_table:
+            sql_str = "select ths_code, ths_maturity_date_sp from {table_name}".format(table_name=table_name)
+            with with_db_session(engine_md) as session:
+                code_in_db_dict = dict(session.execute(sql_str).fetchall())
+                code_in_db_set = set(code_in_db_dict.keys())
+        else:
+            code_in_db_dict, code_in_db_set = {}, set()
+
+        # 查找新增基金
+        code_set_exists = get_private_fund_set(date_end)
+        if code_set_exists is not None:
+            if not refresh and has_table:
+                code_set_exists -= code_in_db_set
+            private_fund_set |= code_set_exists
+
+        # 查找已清盘基金
+        code_set_clear = get_private_fund_set(date_end, field='051010005')
+        if code_set_clear is not None:
+            if not refresh and has_table:
+                code_set_clear -= set([key for key, val in code_in_db_dict.items() if val is not None])
+            private_fund_set |= code_set_clear
 
         ths_code = list(private_fund_set)
         if DEBUG:
@@ -104,22 +125,12 @@ def import_fund_info(ths_code=None, refresh=False):
     indicator, param = unzip_join([(key, val) for key, val, _ in indicator_param_list], sep=';')
     data_df = invoker.THS_BasicData(ths_code, indicator, param, max_code_num=8000)
     if data_df is None or data_df.shape[0] == 0:
-        logging.info("没有可用的 stock info 可以更新")
+        logging.info("没有可用的数据可以更新")
         return
 
-    # 删除历史数据，更新数据
-    if has_table:
-        with with_db_session(engine_md) as session:
-            session.execute(
-                "DELETE FROM {table_name} WHERE ths_code IN (".format(table_name=table_name) + ','.join(
-                    [':code%d' % n for n in range(len(private_fund_set))]
-                ) + ")",
-                params={'code%d' % n: val for n, val in enumerate(private_fund_set)})
-            session.commit()
     dtype = {key: val for key, _, val in indicator_param_list}
     dtype['ths_code'] = String(20)
-    data_count = data_df.shape[0]
-    data_df.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+    data_count = bunch_insert_on_duplicate_update(data_df, table_name, engine_md, dtype)
     logging.info("更新 %s 完成 存量数据 %d 条", table_name, data_count)
     if not has_table:
         alter_table_2_myisam(engine_md, [table_name])
@@ -205,16 +216,18 @@ def import_private_fund_daily(ths_code_set: set = None, begin_time=None):
             # 大于阀值有开始插入
             if data_count >= 10000:
                 tot_data_df = pd.concat(data_df_list)
-                tot_data_df.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+                # tot_data_df.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+                data_count = bunch_insert_on_duplicate_update(tot_data_df, table_name, engine_md, dtype)
                 tot_data_count += data_count
                 data_df_list, data_count = [], 0
 
-            if DEBUG and len(data_df_list) >1:
+            if DEBUG and len(data_df_list) > 1:
                 break
     finally:
         if data_count > 0:
             tot_data_df = pd.concat(data_df_list)
-            tot_data_df.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+            # tot_data_df.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+            data_count = bunch_insert_on_duplicate_update(tot_data_df, table_name, engine_md, dtype)
             tot_data_count += data_count
 
         logging.info("更新 %s 完成 新增数据 %d 条", table_name, tot_data_count)
