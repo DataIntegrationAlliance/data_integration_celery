8c5175478175706db80d62899d54020040a8cb12
mmmaaaggg <mmmaaaggg@163.com>
2018-09-06 15:39:54 +0800

    增加 import_coinbar_on_freq_min 函数，支持分钟数据导入

--- tasks/tushare/coin.py
+++ tasks/tushare/coin.py
@@ -10,9 +10,9 @@
 import pandas as pd
 import tushare as ts
 from tasks import app
-from tasks.utils.fh_utils import date_2_str, str_2_date
+from tasks.utils.fh_utils import date_2_str, str_2_date, str_2_datetime, try_n_times
 from tasks.config import config
-from sqlalchemy.types import String, Date, Integer
+from sqlalchemy.types import String, Date, Integer, DateTime
 from sqlalchemy.dialects.mysql import DOUBLE
 from tasks.utils.db_utils import with_db_session, bunch_insert_on_duplicate_update, alter_table_2_myisam
 from tasks.backend import engine_md
@@ -24,6 +24,12 @@ logger = logging.getLogger()
 ts.set_token(config.TUSHARE_TOKEN)
 pro = ts.pro_api()
 DATE_FORMAT_STR = '%Y%m%d'
+
+
+# @try_n_times(3, sleep_time=0, logger=logger, exception_sleep_time=60)
+# def pro_coinbar(exchange, symbol, freq, start_date, end_date):
+#     data_df = pro.coinbar(exchange=exchange, symbol=symbol, freq=freq, start_date=start_date, end_date=end_date)
+#     return data_df
 
 
 @app.task
@@ -245,10 +251,9 @@ def import_coin_pair_info():
             data_df = pro.coinpair(exchange=exchange, trade_date='20180802')
             if data_df is not None and data_df.shape[0] > 0:
                 data_df.drop('trade_date', axis=1, inplace=True)
-                data_df['trade_date_latest_daily'] = None
-                data_df['delist_date_daily'] = None
-                data_df['trade_date_latest_week'] = None
-                data_df['delist_date_week'] = None
+                for freq in ['daily', 'week', '1min', '5min', '15min', '30min', '60min']:
+                    data_df['trade_date_latest_{freq}'.format(freq=freq)] = None
+                    data_df['delist_date_{freq}'.format(freq=freq)] = None
                 data_count = bunch_insert_on_duplicate_update(data_df, table_name, engine_md, dtype)
                 logging.info("%d/%d) 更新 %s 完成 新增 %s 数据 %d 条",
                              num, exchange_count, table_name, exchange, data_count)
@@ -269,10 +274,10 @@ def import_coinbar():
     freq_list = ['daily', 'week']
     for freq in freq_list:
         import_coinbar_on_freq_daily(freq)
-    # 日内数据 tushare 服务期压力问题，暂停开放
-    # freq_list = ['1min', '5min', '15min', '30min', '60min']
-    # for freq in freq_list:
-    #     import_coinbar_on_freq_min(freq)
+    # 日内数据 tushare 服务器压力问题，暂停开放 '1min', '5min'
+    freq_list = ['15min', '30min', '60min']  # '1min', '5min',
+    for freq in freq_list:
+        import_coinbar_on_freq_min(freq)
 
 
 def import_coinbar_on_freq_daily(freq, code_set=None, base_begin_time=None):
@@ -435,6 +440,177 @@ def import_coinbar_on_freq_daily(freq, code_set=None, base_begin_time=None):
                 CHANGE COLUMN `symbol` `symbol` VARCHAR(60) NOT NULL AFTER `exchange`,
                 CHANGE COLUMN `date` `date` DATE NOT NULL AFTER `symbol`,
                 ADD PRIMARY KEY (`exchange`, `symbol`, `date`)""".format(table_name=table_name)
+            with with_db_session(engine_md) as session:
+                session.execute(create_pk_str)
+
+        logging.info("更新 %s 完成 新增数据 %d 条", table_name, tot_data_count)
+
+
+def import_coinbar_on_freq_min(freq, code_set=None, base_begin_time=None):
+    """
+    抓取 日级别以上数据[ 60min, 30min, 15min, 5min, 1min ]级别
+    :param freq:
+    :param code_set:
+    :param base_begin_time:
+    :return:
+    """
+    if base_begin_time is not None and not isinstance(base_begin_time, date):
+        base_begin_time = str_2_date(base_begin_time)
+    table_name = 'tushare_coin_md_' + freq
+    info_table_name = 'tushare_coin_pair_info'
+    has_table = engine_md.has_table(table_name)
+    if has_table:
+        sql_str = """SELECT exchange, exchange_pair, date_frm, 
+                if(delist_date<end_date, delist_date, end_date) date_to
+            FROM
+            (
+                SELECT info.exchange, info.exchange_pair, 
+                    ifnull(trade_date_max_1, adddate(trade_date_latest,1)) date_frm, 
+                    delist_date,
+                    if(hour(now())<8, subdate(curdate(),2), subdate(curdate(),1)) end_date
+                FROM 
+                (
+                    select exchange, exchange_pair,
+                    ifnull(trade_date_latest_{freq},'2010-01-01') trade_date_latest,
+                    delist_date_{freq} delist_date
+                    from {info_table_name}
+                ) info
+                LEFT OUTER JOIN
+                    (SELECT exchange, symbol, adddate(max(`date`),1) trade_date_max_1 
+                     FROM {table_name} GROUP BY exchange, symbol) daily
+                ON info.exchange = daily.exchange
+                AND info.exchange_pair = daily.symbol
+            ) tt
+            WHERE date_frm <= if(delist_date<end_date, delist_date, end_date) 
+            ORDER BY exchange, exchange_pair""".format(
+            table_name=table_name, info_table_name=info_table_name, freq=freq)
+    else:
+        sql_str = """SELECT exchange, exchange_pair, date_frm, 
+            if(delist_date<end_date, delist_date, end_date) date_to
+        FROM
+        (
+            SELECT exchange, exchange_pair, 
+                ifnull(trade_date_latest_{freq},date('2010-01-01')) date_frm, 
+                delist_date_{freq} delist_date, 
+                if(hour(now())<8, subdate(curdate(),2), subdate(curdate(),1)) end_date
+            FROM {info_table_name} info 
+            ORDER BY exchange, exchange_pair
+        ) tt
+        WHERE date_frm <= if(delist_date<end_date, delist_date, end_date) 
+        ORDER BY exchange, exchange_pair""".format(info_table_name=info_table_name, freq=freq)
+        logger.warning('%s 不存在，仅使用 %s 表进行计算日期范围', table_name, info_table_name)
+
+    with with_db_session(engine_md) as session:
+        # 获取每只股票需要获取日线数据的日期区间
+        table = session.execute(sql_str)
+        # 获取每只股票需要获取日线数据的日期区间
+        code_date_range_dic = {
+            (exchange, symbol):
+                (date_from if base_begin_time is None else min([date_from, base_begin_time]),
+                 date_to)
+            for exchange, symbol, date_from, date_to in table.fetchall() if
+            code_set is None or (exchange, symbol) in code_set}
+
+    # 设置 dtype
+    dtype = {
+        'exchange': String(60),
+        'symbol': String(60),
+        'date': Date,
+        'datetime': DateTime,
+        'open': DOUBLE,
+        'high': DOUBLE,
+        'low': DOUBLE,
+        'close': DOUBLE,
+        'vol': DOUBLE,
+    }
+
+    # 更新 info 表 trade_date_latest 字段
+    trade_date_latest_list = []
+    update_trade_date_latest_str = """update tushare_coin_pair_info info
+        set info.trade_date_latest_daily = :trade_date_latest
+        where info.exchange = :exchange and exchange_pair=:exchange_pair"""
+
+    data_df_list, data_count, tot_data_count, code_count = [], 0, 0, len(code_date_range_dic)
+    try:
+        for num, ((exchange, exchange_pair), (begin_time, end_time)) in enumerate(code_date_range_dic.items(), start=1):
+            begin_time_str = date_2_str(begin_time, DATE_FORMAT_STR)
+            end_time_str = date_2_str(end_time, DATE_FORMAT_STR)
+            logger.debug('%d/%d) %s %s [%s - %s]', num, code_count, exchange, exchange_pair, begin_time, end_time)
+            try:
+                # data_df = pro.coinbar(exchange='huobi', symbol='gxsbtc', freq='1min', start_date='20180701', end_date='20180801')
+                data_df = pro.coinbar(exchange=exchange, symbol=exchange_pair, freq=freq,
+                                      start_date=begin_time_str,
+                                      end_date=end_time_str)
+            except Exception as exp:
+                if len(exp.args) >= 1 and exp.args[0] == '系统内部错误':
+                    trade_date_latest_list.append({
+                        'exchange': exchange,
+                        'exchange_pair': exchange_pair,
+                        'trade_date_latest': '2020-02-02',
+                    })
+                    logger.warning(
+                        "coinbar(exchange='%s', symbol='%s', freq='%s', start_date='%s', end_date='%s') 系统内部错误",
+                        exchange, exchange_pair, freq, begin_time_str, end_time_str)
+                    continue
+                logger.exception("coinbar(exchange='%s', symbol='%s', freq='%s', start_date='%s', end_date='%s')",
+                                 exchange, exchange_pair, freq, begin_time_str, end_time_str)
+                raise exp from exp
+
+            if data_df is not None and data_df.shape[0] > 0:
+                data_count += data_df.shape[0]
+                data_df['exchange'] = exchange
+                data_df['datetime'] = data_df['date']
+                data_df['date'] = data_df['date'].apply(lambda x: str_2_datetime(x).date())
+                data_df_list.append(data_df)
+
+            # 记录最新交易日变化
+            trade_date_latest_list.append({
+                'exchange': exchange,
+                'exchange_pair': exchange_pair,
+                'trade_date_latest': end_time_str,
+            })
+            # 大于阀值有开始插入
+            if data_count >= 10000:
+                data_df_all = pd.concat(data_df_list)
+                # data_df_all.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+                data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md, dtype)
+                tot_data_count += data_count
+                data_df_list, data_count = [], 0
+
+                # 更新 info 表 trade_date_latest 字段
+                with with_db_session(engine_md) as session:
+                    result = session.execute(update_trade_date_latest_str, params=trade_date_latest_list)
+                    update_count = result.rowcount
+                    session.commit()
+                    logger.info('更新 %d 条交易对的最新交易 %s 信息', update_count, freq)
+                trade_date_latest_list = []
+
+            # 仅调试使用
+            if DEBUG and len(data_df_list) > 1:
+                break
+    finally:
+        if data_count > 0:
+            data_df_all = pd.concat(data_df_list)
+            # data_df_all.to_sql(table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md, dtype)
+            tot_data_count += data_count
+
+        # 更新 info 表 trade_date_latest 字段
+        if len(trade_date_latest_list) > 0:
+            with with_db_session(engine_md) as session:
+                result = session.execute(update_trade_date_latest_str, params=trade_date_latest_list)
+                update_count = result.rowcount
+                session.commit()
+                logger.info('更新 %d 条交易对的最新交易日信息', update_count)
+
+        if not has_table and engine_md.has_table(table_name):
+            alter_table_2_myisam(engine_md, [table_name])
+            # build_primary_key([table_name])
+            create_pk_str = """ALTER TABLE {table_name}
+                CHANGE COLUMN `exchange` `exchange` VARCHAR(60) NOT NULL FIRST,
+                CHANGE COLUMN `symbol` `symbol` VARCHAR(60) NOT NULL AFTER `exchange`,
+                CHANGE COLUMN `datetime` `datetime` DATETIME NOT NULL AFTER `symbol`,
+                ADD PRIMARY KEY (`exchange`, `symbol`, `datetime`)""".format(table_name=table_name)
             with with_db_session(engine_md) as session:
                 session.execute(create_pk_str)
 
