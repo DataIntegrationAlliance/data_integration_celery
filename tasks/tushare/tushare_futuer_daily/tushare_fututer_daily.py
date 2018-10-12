2f2659adadf1008c3b0a053582ef258bcbfe691e
yangxiaobao87 <ybychem@gmail.com>
2018-10-12 13:23:53 +0800

    tushare 期货

--- tasks/tushare/tushare_futuer_daily/tushare_fututer_daily.py
+++ tasks/tushare/tushare_futuer_daily/tushare_fututer_daily.py
@@ -1,4 +1,29 @@
-import tushare as ts
-cons = ts.get_apis()
-df_day =ts.bar("RB1801",conn=cons,asset='X',freq='D')
-df_5min =ts.bar("IF1801",conn=cons,asset='X',freq='5min')
+"""
+Created on 2018/10/12
+@author: yby
+@desc    : 2018/10/12 已经正式运行测试完成，可以正常使用
+"""
+import pandas as pd
+import logging
+from tasks.backend.orm import build_primary_key
+from datetime import date, datetime, timedelta
+from tasks.utils.fh_utils import try_2_date, STR_FORMAT_DATE, datetime_2_str, split_chunk
+from tasks import app
+from sqlalchemy.types import String, Date, Integer
+from sqlalchemy.dialects.mysql import DOUBLE
+from tasks.backend import engine_md
+from tasks.merge.code_mapping import update_from_info_table
+from tasks.utils.db_utils import with_db_session, add_col_2_table, alter_table_2_myisam, \
+    bunch_insert_on_duplicate_update
+from tasks.tushare.ts_pro_api import pro
+
+DEBUG = False
+logger = logging.getLogger()
+DATE_BASE = datetime.strptime('2005-01-01', STR_FORMAT_DATE).date()
+ONE_DAY = timedelta(days=1)
+# 标示每天几点以后下载当日行情数据
+BASE_LINE_HOUR = 16
+STR_FORMAT_DATE_TS = '%Y%m%d'
+
+
+ts.pro_bar(pro_api=pro,ts_code='000001.SH',asset='I',  start_date='20180101', end_date='20181011')
