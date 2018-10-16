60307d081097f7223129bc1aa6661f8c8d3b00cf
yangxiaobao87 <ybychem@gmail.com>
2018-10-16 15:19:07 +0800

    质押明细

--- tasks/tushare/tushare_fina_reports/pledge_detail.py
+++ tasks/tushare/tushare_fina_reports/pledge_detail.py
@@ -5,14 +5,8 @@ Created on 2018/9/3
 contact author:ybychem@gmail.com
 """
 
-"""
-Created on 2018/9/3
-@author: yby
-@desc    : 2018-09-3
-contact author:ybychem@gmail.com
-"""
-
 import pandas as pd
+import numpy as np
 import logging
 from tasks.backend.orm import build_primary_key
 from datetime import date, datetime, timedelta
@@ -34,33 +28,31 @@ ONE_DAY = timedelta(days=1)
 BASE_LINE_HOUR = 16
 STR_FORMAT_DATE_TS = '%Y%m%d'
 
-INDICATOR_PARAM_LIST_TUSHARE_STOCK_PLEDGE_STAT = [
+INDICATOR_PARAM_LIST_TUSHARE_STOCK_PLEDGE_DETAIL = [
     ('ts_code', String(20)),
     ('ann_date', Date),
-    ('holder_name', String(20)),
+    ('holder_name', String(200)),
     ('pledge_amount', DOUBLE),
     ('start_date', Date),
     ('end_date', Date),
     ('is_release', String(20)),
     ('release_date', Date),
-    ('pledgor', String(20)),
+    ('pledgor', String(200)),
     ('holding_amount', DOUBLE),
     ('pledged_amount', DOUBLE),
     ('p_total_ratio', DOUBLE),
     ('h_total_ratio', DOUBLE),
     ('is_buyback', String(20)),
-    ('desc', Text),
-
 ]
 # 设置 dtype
-DTYPE_TUSHARE_STOCK_PLEDGE_STAT = {key: val for key, val in INDICATOR_PARAM_LIST_TUSHARE_STOCK_PLEDGE_STAT}
+DTYPE_TUSHARE_STOCK_PLEDGE_DETAIL = {key: val for key, val in INDICATOR_PARAM_LIST_TUSHARE_STOCK_PLEDGE_DETAIL}
 
-@try_n_times(times=5, sleep_time=5, logger=logger, exception=Exception, exception_sleep_time=5)
+@try_n_times(times=5, sleep_time=2, logger=logger, exception=Exception, exception_sleep_time=5)
 def invoke_pledge_detail(ts_code):
     invoke_pledge_detail = pro.pledge_detail(ts_code=ts_code)
     return invoke_pledge_detail
 
-df=invoke_pledge_detail(ts_code='000014.SZ')
+# df=invoke_pledge_detail(ts_code='000014.SZ')
 
 @app.task
 def import_tushare_stock_pledge_detail(chain_param=None,ts_code_set=None):
@@ -75,7 +67,7 @@ def import_tushare_stock_pledge_detail(chain_param=None,ts_code_set=None):
     has_table = engine_md.has_table(table_name)
     # 进行表格判断，确定是否含有tushare_stock_daily
 
-    sql_str = """SELECT ts_code FROM tushare_stock_info """
+    sql_str = """SELECT ts_code FROM tushare_stock_info where ts_code >'002021.SZ'"""
     logger.warning('使用 tushare_stock_info 表确认需要提取股票质押数据的范围')
 
     with with_db_session(engine_md) as session:
@@ -91,6 +83,15 @@ def import_tushare_stock_pledge_detail(chain_param=None,ts_code_set=None):
     try:
         for ts_code in ts_code_list:
             data_df = invoke_pledge_detail(ts_code=ts_code)
+            for i in range(len(data_df.start_date)):
+                if data_df.start_date[i] is not None and len(data_df.start_date[i]) != 8:
+                    data_df.start_date[i] = np.nan
+            for i in range(len(data_df.end_date)):
+                if data_df.end_date[i] is not None and len(data_df.end_date[i]) != 8:
+                    data_df.end_date[i] = np.nan
+            for i in range(len(data_df.release_date)):
+                if data_df.release_date[i] is not None and len(data_df.release_date[i]) != 8:
+                    data_df.release_date[i] = np.nan
             logger.warning('提取 %s 质押信息 %d 条',ts_code, len(data_df))
 
             # 把数据攒起来
@@ -98,9 +99,9 @@ def import_tushare_stock_pledge_detail(chain_param=None,ts_code_set=None):
                 data_count += data_df.shape[0]
                 data_df_list.append(data_df)
             # 大于阀值有开始插入
-            if data_count >= 10000 and len(data_df_list)>0 :
+            if data_count >= 100 and len(data_df_list)>0 :
                 data_df_all = pd.concat(data_df_list)
-                data_count=bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md, DTYPE_TUSHARE_STOCK_PLEDGE_STAT)
+                data_count=bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md, DTYPE_TUSHARE_STOCK_PLEDGE_DETAIL)
                 logger.warning('更新股票质押信息 %d 条',data_count)
                 all_data_count += data_count
                 data_df_list, data_count = [], 0
@@ -112,7 +113,7 @@ def import_tushare_stock_pledge_detail(chain_param=None,ts_code_set=None):
     finally:
         if len(data_df_list) > 0:
             data_df_all = pd.concat(data_df_list)
-            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_PLEDGE_STAT)
+            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_PLEDGE_DETAIL)
             all_data_count = all_data_count + data_count
             logging.info("更新 %s 结束，总共 %d 条信息被更新", table_name, all_data_count)
 
@@ -121,3 +122,5 @@ if __name__ == "__main__":
     # DEBUG = True
 
     import_tushare_stock_pledge_detail()
+
+
