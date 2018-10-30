3ca202bbd9c76df238450b18c3a08d7a68889f9b
Yangbaoyuan <ybychem@gmail.com>
2018-10-30 13:17:26 +0800

    业绩预告和业绩快报

--- tasks/tushare/tushare_fina_reports/forecast.py
+++ tasks/tushare/tushare_fina_reports/forecast.py
@@ -1,4 +1,11 @@
 """
+Created on 2018/9/3
+@author: yby
+@desc    : 2018-09-3
+contact author:ybychem@gmail.com
+"""
+
+"""
 Created on 2018/10/30
 @author: yby
 @desc    : 2018-10-30
@@ -28,58 +35,38 @@ ONE_DAY = timedelta(days=1)
 BASE_LINE_HOUR = 16
 STR_FORMAT_DATE_TS = '%Y%m%d'
 
-INDICATOR_PARAM_LIST_TUSHARE_STOCK_EXPRESS = [
+INDICATOR_PARAM_LIST_TUSHARE_STOCK_FORECAST = [
     ('ts_code', String(20)),
     ('ann_date', Date),
     ('end_date', Date),
-    ('revenue', DOUBLE),
-    ('operate_profit', DOUBLE),
-    ('total_profit', DOUBLE),
-    ('n_income', DOUBLE),
-    ('total_assets', DOUBLE),
-    ('total_hldr_eqy_exc_min_int', DOUBLE),
-    ('diluted_eps', DOUBLE),
-    ('diluted_roe', DOUBLE),
-    ('yoy_net_profit', DOUBLE),
-    ('bps', DOUBLE),
-    ('yoy_sales', DOUBLE),
-    ('yoy_op', DOUBLE),
-    ('yoy_tp', DOUBLE),
-    ('yoy_dedu_np', DOUBLE),
-    ('yoy_eps', DOUBLE),
-    ('yoy_roe', DOUBLE),
-    ('growth_assets', DOUBLE),
-    ('yoy_equity', DOUBLE),
-    ('growth_bps', DOUBLE),
-    ('or_last_year', DOUBLE),
-    ('op_last_year', DOUBLE),
-    ('tp_last_year', DOUBLE),
-    ('np_last_year', DOUBLE),
-    ('eps_last_year', DOUBLE),
-    ('open_net_assets', DOUBLE),
-    ('open_bps', DOUBLE),
-    ('perf_summary', Text),
-    ('is_audit', TINYINT),
-    ('remark', Text),
+    ('type', Text),
+    ('p_change_min', DOUBLE),
+    ('p_change_max', DOUBLE),
+    ('net_profit_min', DOUBLE),
+    ('net_profit_max', DOUBLE),
+    ('last_parent_net', DOUBLE),
+    ('first_ann_date', Date),
+    ('summary', Text),
+    ('change_reason', Text),
 ]
 # 设置 dtype
-DTYPE_TUSHARE_STOCK_EXPRESS = {key: val for key, val in INDICATOR_PARAM_LIST_TUSHARE_STOCK_EXPRESS}
+DTYPE_TUSHARE_STOCK_FORECAST = {key: val for key, val in INDICATOR_PARAM_LIST_TUSHARE_STOCK_FORECAST}
 
 
 @try_n_times(times=5, sleep_time=2, logger=logger, exception=Exception, exception_sleep_time=5)
-def invoke_express(ts_code, start_date, end_date):
-    invoke_express = pro.express(ts_code=ts_code, start_date=start_date, end_date=end_date)
-    return invoke_express
+def invoke_forecast(ts_code, start_date, end_date):
+    invoke_forecast = pro.forecast(ts_code=ts_code, start_date=start_date, end_date=end_date)
+    return invoke_forecast
 
 
 @app.task
-def import_tushare_stock_express(chain_param=None, ts_code_set=None):
+def import_tushare_stock_forecast(chain_param=None, ts_code_set=None):
     """
     插入股票日线数据到最近一个工作日-1。
     如果超过 BASE_LINE_HOUR 时间，则获取当日的数据
     :return:
     """
-    table_name = 'tushare_stock_express'
+    table_name = 'tushare_stock_forecast'
     logging.info("更新 %s 开始", table_name)
 
     has_table = engine_md.has_table(table_name)
@@ -95,8 +82,8 @@ def import_tushare_stock_express(chain_param=None, ts_code_set=None):
                   tushare_stock_info info 
                 LEFT OUTER JOIN
                     (SELECT ts_code, adddate(max(ann_date),1) ann_date 
-                    FROM {table_name} GROUP BY ts_code) express
-                ON info.ts_code = express.ts_code
+                    FROM {table_name} GROUP BY ts_code) forecast
+                ON info.ts_code = forecast.ts_code
             ) tt
             WHERE date_frm <= if(delist_date<end_date, delist_date, end_date) 
             ORDER BY ts_code""".format(table_name=table_name)
@@ -125,21 +112,21 @@ def import_tushare_stock_express(chain_param=None, ts_code_set=None):
             ts_code_set is None or ts_code in ts_code_set}
 
     data_df_list, data_count, all_data_count, data_len = [], 0, 0, len(code_date_range_dic)
-    logger.info('%d stock balancesheets will been import into tushare_stock_express', data_len)
+    logger.info('%d stock balancesheets will been import into tushare_stock_forcast', data_len)
     # 将data_df数据，添加到data_df_list
 
     Cycles = 1
     try:
         for num, (ts_code, (date_from, date_to)) in enumerate(code_date_range_dic.items(), start=1):
             logger.debug('%d/%d) %s [%s - %s]', num, data_len, ts_code, date_from, date_to)
-            data_df = invoke_express(ts_code=ts_code, start_date=datetime_2_str(date_from, STR_FORMAT_DATE_TS),
+            data_df = invoke_forecast(ts_code=ts_code, start_date=datetime_2_str(date_from, STR_FORMAT_DATE_TS),
                                      end_date=datetime_2_str(date_to, STR_FORMAT_DATE_TS))
 
             if data_df is None:
                 logger.warning('%d/%d) %s has no data during %s %s', num, data_len, ts_code, date_from, date_to)
                 continue
             elif data_df is not None:
-                logger.info('整体进度：%d/%d)， %d 条 %s 业绩快报数据被提取，起止时间为 %s 和 %s', num, data_len, data_df.shape[0], ts_code,date_from, date_to)
+                logger.info('整体进度：%d/%d)， %d 条 %s 业绩预告数据被提取，起止时间为 %s 和 %s', num, data_len, data_df.shape[0], ts_code,date_from, date_to)
 
             # 把数据攒起来
             if data_df is not None and data_df.shape[0] > 0:
@@ -149,8 +136,8 @@ def import_tushare_stock_express(chain_param=None, ts_code_set=None):
             # 大于阀值有开始插入
             if data_count >= 1000 and len(data_df_list) > 0:
                 data_df_all = pd.concat(data_df_list)
-                bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_EXPRESS)
-                logger.info('%d 条业绩快报数据被插入 %s 表', data_count, table_name)
+                bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_FORECAST)
+                logger.info('%d 条业绩预告数据被插入 %s 表', data_count, table_name)
                 all_data_count += data_count
                 data_df_list, data_count = [], 0
                 # # 数据插入数据库
@@ -166,19 +153,19 @@ def import_tushare_stock_express(chain_param=None, ts_code_set=None):
         # 导入数据库
         if len(data_df_list) > 0:
             data_df_all = pd.concat(data_df_list)
-            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_EXPRESS)
+            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_FORECAST)
             all_data_count = all_data_count + data_count
-            logging.info("更新 %s 结束 %d 条业绩快报信息被更新", table_name, all_data_count)
+            logging.info("更新 %s 结束 %d 条业绩预告信息被更新", table_name, all_data_count)
             # if not has_table and engine_md.has_table(table_name):
             #     alter_table_2_myisam(engine_md, [table_name])
             #     build_primary_key([table_name])
 
 
 if __name__ == "__main__":
-    # DEBUG = True
+    DEBUG = True
     # import_tushare_stock_info(refresh=False)
     # 更新每日股票数据
-    import_tushare_stock_express()
+    import_tushare_stock_forecast()
 
     # 去除重复数据用的
     # sql_str = """SELECT * FROM old_tushare_stock_balancesheet """
