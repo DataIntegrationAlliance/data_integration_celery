a1b5be721b6ab389eda8b10f44ab040633a3b205
mmmaaaggg <mmmaaaggg@163.com>
2018-11-05 18:28:20 +0800

    add repair table

---
+++ tasks/backend/repair_table.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/11/2 17:26
+@File    : repair_table.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+from tasks.backend import engine_md
+from tasks.utils.db_utils import execute_sql, with_db_session
+from datetime import datetime
+
+
+def repair_table():
+    datetime_start = datetime.now()
+    with with_db_session(engine_md) as session:
+        session.execute('REPAIR TABLE pytdx_stock_tick USE_FRM')
+
+    datetime_end = datetime.now()
+    span = datetime_end - datetime_start
+    print('花费时间 ', span)
+
+
+if __name__ == "__main__":
+    repair_table()
