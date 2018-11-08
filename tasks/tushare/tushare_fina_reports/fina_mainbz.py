c25ea2c0f2e69482443e7ceb3f7ab69be6030448
Yangbaoyuan <ybychem@gmail.com>
2018-11-08 10:11:34 +0800

    bug repair

--- tasks/tushare/tushare_fina_reports/fina_mainbz.py
+++ tasks/tushare/tushare_fina_reports/fina_mainbz.py
@@ -25,6 +25,19 @@ ONE_DAY = timedelta(days=1)
 BASE_LINE_HOUR = 16
 STR_FORMAT_DATE_TS = '%Y%m%d'
 
+INDICATOR_PARAM_LIST_TUSHARE_STOCK_FINA_MAINBZ = [
+    ('ts_code', String(20)),
+    ('end_date', Date),
+    ('bz_item', String(200)),
+    ('bz_sales', DOUBLE),
+    ('bz_profit', DOUBLE),
+    ('bz_cost', DOUBLE),
+    ('curr_type', String(20)),
+    ('update_flag', String(20)),
+    ('market_type', String(20)),
+]
+# 设置 dtype
+DTYPE_TUSHARE_STOCK_FINA_MAINBZ = {key: val for key, val in INDICATOR_PARAM_LIST_TUSHARE_STOCK_FINA_MAINBZ}
 
 @try_n_times(times=3, sleep_time=2, logger=logger, exception=Exception, exception_sleep_time=10)
 def invoke_fina_mainbz(ts_code, start_date, end_date, type):
@@ -33,7 +46,7 @@ def invoke_fina_mainbz(ts_code, start_date, end_date, type):
 
 
 @app.task
-def import_tushare_stock_fina_mainbz(ts_code_set, chain_param=None):
+def import_tushare_stock_fina_mainbz(chain_param=None,ts_code_set=None):
     """
     插入股票日线数据到最近一个工作日-1。
     如果超过 BASE_LINE_HOUR 时间，则获取当日的数据
@@ -41,17 +54,7 @@ def import_tushare_stock_fina_mainbz(ts_code_set, chain_param=None):
     """
     table_name = 'tushare_stock_fin_mainbz'
     logging.info("更新 %s 开始", table_name)
-    param_list = [
-        ('ts_code', String(20)),
-        ('end_date', Date),
-        ('bz_item', String(200)),
-        ('bz_sales', DOUBLE),
-        ('bz_profit', DOUBLE),
-        ('bz_cost', DOUBLE),
-        ('curr_type', String(20)),
-        ('update_flag', String(20)),
-        ('market_type', String(20)),
-    ]
+
 
     has_table = engine_md.has_table(table_name)
     # 进行表格判断，确定是否含有tushare_stock_daily
@@ -95,11 +98,11 @@ def import_tushare_stock_fina_mainbz(ts_code_set, chain_param=None):
             for ts_code, date_from, date_to in table.fetchall() if
             ts_code_set is None or ts_code in ts_code_set}
     # 设置 dtype
-    dtype = {key: val for key, val in param_list}
+    # dtype = {key: val for key, val in param_list}
     # dtype['ts_code'] = String(20)
     # dtype['trade_date'] = Date
 
-    data_len = len(code_date_range_dic)
+    data_df_list, data_count, all_data_count, data_len = [], 0, 0, len(code_date_range_dic)
     logger.info('%d data will been import into %s', data_len, table_name)
     # 将data_df数据，添加到data_df_list
 
@@ -108,46 +111,59 @@ def import_tushare_stock_fina_mainbz(ts_code_set, chain_param=None):
         for num, (ts_code, (date_from, date_to)) in enumerate(code_date_range_dic.items(), start=1):
             for mainbz_type in list(['P', 'D']):
                 logger.debug('%d/%d) %s [%s - %s] %s', num, data_len, ts_code, date_from, date_to, mainbz_type)
-                df = invoke_fina_mainbz(ts_code=ts_code, start_date=datetime_2_str(date_from, STR_FORMAT_DATE_TS),
+                data_df = invoke_fina_mainbz(ts_code=ts_code, start_date=datetime_2_str(date_from, STR_FORMAT_DATE_TS),
                                         end_date=datetime_2_str(date_to, STR_FORMAT_DATE_TS), type=mainbz_type)
-                df['market_type'] = mainbz_type
+                data_df['market_type'] = mainbz_type
                 # logger.info(' %d data of %s between %s and %s', df.shape[0], ts_code, date_from, date_to)
-                data_df = df
-                if len(data_df) > 0:
-                    while try_2_date(df['end_date'].iloc[-1]) > date_from:
-                        last_date_in_df_last, last_date_in_df_cur = try_2_date(df['end_date'].iloc[-1]), None
+                # data_df = df
+                if len(data_df) > 0 and data_df['ann_date'].iloc[-1] is not None:
+                    while try_2_date(data_df['end_date'].iloc[-1]) > date_from:
+                        last_date_in_df_last, last_date_in_df_cur = try_2_date(data_df['end_date'].iloc[-1]), None
                         df2 = invoke_fina_mainbz(ts_code=ts_code,
                                                  start_date=datetime_2_str(date_from, STR_FORMAT_DATE_TS),
-                                                 end_date=datetime_2_str(try_2_date(df['end_date'].iloc[-1]),
+                                                 end_date=datetime_2_str(try_2_date(data_df['end_date'].iloc[-1]),
                                                                          STR_FORMAT_DATE_TS), type=mainbz_type)
                         df2['market_type'] = mainbz_type
-                        if len(df2) > 0:
+                        if len(df2) > 0 and df2['ann_date'].iloc[-1] is None:
                             last_date_in_df_cur = try_2_date(df2['end_date'].iloc[-1])
                             if last_date_in_df_cur < last_date_in_df_last:
                                 data_df = pd.concat([data_df, df2])
-                                df = df2
+                                # df = df2
                             elif last_date_in_df_cur <= last_date_in_df_last:
                                 break
-                            if data_df is None:
-                                logger.warning('%d/%d) %s has no data during %s %s', num, data_len, ts_code, date_from,
-                                               date_to)
-                                continue
-                            logger.info('%d/%d) %d data of %s between %s and %s', num, data_len, data_df.shape[0],
-                                        ts_code, date_from, date_to)
+
                         elif len(df2) <= 0:
                             break
-                    # 数据插入数据库
-                    data_count = bunch_insert_on_duplicate_update(data_df, table_name, engine_md, dtype)
-                    logging.info("更新 %s 结束 %d 条信息被更新", table_name, data_count)
+                # 数据插入数据库
+                if data_df is None:
+                    logger.warning('%d/%d) %s has no data during %s %s', num, data_len, ts_code, date_from,
+                                   date_to)
+                    continue
+                elif data_df is not None:
+                    logger.info('整体进度：%d/%d)， %d 条  %s 的 %s类型统计之主营业务数据被提取，起止时间为 %s 和 %s', num, data_len,
+                                data_df.shape[0], ts_code, mainbz_type, date_from, date_to)
+
+                # 把数据攒起来
+                if data_df is not None and data_df.shape[0] > 0:
+                    data_count += data_df.shape[0]
+                    data_df_list.append(data_df)
+                # 大于阀值有开始插入
+                if data_count >= 500 and len(data_df_list) > 0:
+                    data_df_all = pd.concat(data_df_list)
+                    bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md,DTYPE_TUSHARE_STOCK_FINA_MAINBZ)
+                    all_data_count += data_count
+                    data_df_list, data_count = [], 0
             # 仅调试使用
             Cycles = Cycles + 1
             if DEBUG and Cycles > 2:
                 break
     finally:
         # 导入数据库
-        if len(data_df) > 0:
-            data_count = bunch_insert_on_duplicate_update(data_df, table_name, engine_md, dtype)
-            logging.info("更新 %s 结束 %d 条信息被更新", table_name, data_count)
+        if len(data_df_list) > 0:
+            data_df_all = pd.concat(data_df_list)
+            data_count = bunch_insert_on_duplicate_update(data_df_all, table_name, engine_md, DTYPE_TUSHARE_STOCK_FINA_MAINBZ)
+            all_data_count = all_data_count + data_count
+            logging.info("更新 %s 结束 %d 条主营业务信息被更新", table_name, all_data_count)
             # if not has_table and engine_md.has_table(table_name):
             #     alter_table_2_myisam(engine_md, [table_name])
             #     build_primary_key([table_name])
@@ -157,13 +173,13 @@ if __name__ == "__main__":
     # DEBUG = True
     # import_tushare_stock_info(refresh=False)
     # 更新每日股票数据
-    SQL = """SELECT ts_code FROM tushare_stock_info WHERE ts_code>'603320.SH'"""
-    with with_db_session(engine_md) as session:
-        # 获取每只股票需要获取日线数据的日期区间
-        table = session.execute(SQL)
-        ts_code_set = list([row[0] for row in table.fetchall()])
+    # SQL = """SELECT ts_code FROM tushare_stock_info WHERE ts_code>'603320.SH'"""
+    # with with_db_session(engine_md) as session:
+    #     # 获取每只股票需要获取日线数据的日期区间
+    #     table = session.execute(SQL)
+    #     ts_code_set = list([row[0] for row in table.fetchall()])
 
-    import_tushare_stock_fina_mainbz(ts_code_set)
+    import_tushare_stock_fina_mainbz()
 
     # 测试接口和数据提取用
     # df = invoke_fina_indicator(ts_code='600000.SH',start_date='19980801',end_date='20180820',fields=fields)
