47715628bff01072a9165c6977633cbf8d4244e8
mmmaaaggg <mmmaaaggg@163.com>
2019-01-18 14:22:42 +0800

    enhance reorg logic

--- tasks/wind/future_reorg/reorg_md_2_db.py
+++ tasks/wind/future_reorg/reorg_md_2_db.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on 2018/2/12
-@author: MG
+@author  : MG
+@Time    : 2019/1/14 15:59
+@File    : reorg_md_2_db.py
+@contact : mmmaaaggg@163.com
+@desc    : 从 QABAT 项目迁移来
+将期货行情数据进行整理，相同批准不同合约的行情合并成为连续行情，并进行复权处理，然后保存到数据库
 """
 
 import math, os
@@ -11,8 +16,9 @@ import re
 from sqlalchemy.dialects.mysql import DOUBLE
 from sqlalchemy.types import String, Date, Time, DateTime, Integer
 from tasks.backend import engine_md
-from tasks.utils.db_utils import with_db_session
+from tasks.utils.db_utils import with_db_session, bunch_insert_on_duplicate_update
 from tasks.utils.fh_utils import str_2_float
+from tasks.config import config
 
 logger = logging.getLogger()
 # 对于郑商所部分合约需要进行特殊的名称匹配规则
@@ -86,6 +92,67 @@ def is_earlier_instruments(inst_a, inst_b, by_wind_code=True):
     return inst_num_a < inst_num_b
 
 
+def update_df_2_db(instrument_type, table_name, data_df):
+    """将 DataFrame 数据保存到 数据库对应的表中"""
+    dtype = {
+        'trade_date': Date,
+        'Contract': String(20),
+        'ContractNext': String(20),
+        'Close': DOUBLE,
+        'CloseNext': DOUBLE,
+        'TermStructure': DOUBLE,
+        'Volume': DOUBLE,
+        'VolumeNext': DOUBLE,
+        'OI': DOUBLE,
+        'OINext': DOUBLE,
+        'Open': DOUBLE,
+        'OpenNext': DOUBLE,
+        'High': DOUBLE,
+        'HighNext': DOUBLE,
+        'Low': DOUBLE,
+        'LowNext': DOUBLE,
+        'WarehouseWarrant': DOUBLE,
+        'WarehouseWarrantNext': DOUBLE,
+        'adj_factor_main': DOUBLE,
+        'adj_factor_secondary': DOUBLE,
+        'instrument_type': String(20),
+    }
+    # 为了解决 AttributeError: 'numpy.float64' object has no attribute 'translate' 错误，需要将数据类型转换成 float
+    data_df["Close"] = data_df["Close"].apply(str_2_float)
+    data_df["CloseNext"] = data_df["CloseNext"].apply(str_2_float)
+    data_df["TermStructure"] = data_df["TermStructure"].apply(str_2_float)
+    data_df["Volume"] = data_df["Volume"].apply(str_2_float)
+    data_df["VolumeNext"] = data_df["VolumeNext"].apply(str_2_float)
+    data_df["OI"] = data_df["OI"].apply(str_2_float)
+    data_df["OINext"] = data_df["OINext"].apply(str_2_float)
+    data_df["Open"] = data_df["Open"].apply(str_2_float)
+    data_df["OpenNext"] = data_df["OpenNext"].apply(str_2_float)
+    data_df["High"] = data_df["High"].apply(str_2_float)
+    data_df["HighNext"] = data_df["HighNext"].apply(str_2_float)
+    data_df["Low"] = data_df["Low"].apply(str_2_float)
+    data_df["LowNext"] = data_df["LowNext"].apply(str_2_float)
+    data_df["WarehouseWarrant"] = data_df["WarehouseWarrant"].apply(str_2_float)
+    data_df["WarehouseWarrantNext"] = data_df["WarehouseWarrantNext"].apply(str_2_float)
+    data_df["adj_factor_main"] = data_df["adj_factor_main"].apply(str_2_float)
+    data_df["adj_factor_secondary"] = data_df["adj_factor_secondary"].apply(str_2_float)
+    # 清理历史记录
+    with with_db_session(engine_md) as session:
+        sql_str = """SELECT table_name FROM information_schema.TABLES 
+            WHERE table_name = :table_name and TABLE_SCHEMA=(select database())"""
+        # 复权数据表
+        is_existed = session.execute(sql_str, params={"table_name": table_name}).fetchone()
+        if is_existed is not None:
+            session.execute("delete from %s where instrument_type = :instrument_type" % table_name,
+                            params={"instrument_type": instrument_type})
+            logger.debug("删除 %s 中的 %s 历史数据", table_name, instrument_type)
+
+    # 插入数据库
+    # pd.DataFrame.to_sql(data_df, table_name, engine_md, if_exists='append', index=False, dtype=dtype)
+    bunch_insert_on_duplicate_update(data_df, table_name, engine_md,
+                                     dtype=dtype, myisam_if_create_table=True,
+                                     primary_keys=['trade_date', 'Contract'], schema=config.DB_SCHEMA_MD)
+
+
 def data_reorg_daily(instrument_type, update_table=True) -> (pd.DataFrame, pd.DataFrame):
     """
     将每一个交易日主次合约合约行情信息进行展示
@@ -93,7 +160,8 @@ def data_reorg_daily(instrument_type, update_table=True) -> (pd.DataFrame, pd.Da
     :param update_table: 是否更新数据库
     :return: 
     """
-    sql_str = r"select wind_code, trade_date, open, high, low, close, volume, position, st_stock from wind_future_daily where wind_code regexp %s"
+    sql_str = r"""select wind_code, trade_date, open, high, low, close, volume, position, st_stock 
+      from wind_future_daily where wind_code regexp %s"""
     data_df = pd.read_sql(sql_str, engine_md, params=[r'^%s[0-9]+\.[A-Z]+' % (instrument_type)])
     date_instrument_vol_df = data_df.pivot(index="trade_date", columns="wind_code", values="volume").sort_index()
     date_instrument_open_df = data_df.pivot(index="trade_date", columns="wind_code", values="open")
@@ -196,7 +264,7 @@ def data_reorg_daily(instrument_type, update_table=True) -> (pd.DataFrame, pd.Da
                                trade_date, instrument_id_main, instrument_id_secondary)
                 continue
             date_reorg_data_dic[trade_date] = {
-                "Date": trade_date,
+                "trade_date": trade_date,
                 "Contract": instrument_id_main,
                 "ContractNext": instrument_id_secondary if instrument_id_secondary is not None else None,
                 "Close": close_main,
@@ -227,96 +295,46 @@ def data_reorg_daily(instrument_type, update_table=True) -> (pd.DataFrame, pd.Da
         trade_date_available_list.append(trade_date)
     # 建立DataFrame
     if len(date_reorg_data_dic) == 0:
-        date_noadj_df, date_adj_df = None, None
+        data_no_adj_df, data_adj_df = None, None
     else:
-        date_reorg_data_df = pd.DataFrame(date_reorg_data_dic).T
-        col_list = ["Date", "Contract", "ContractNext", "Close", "CloseNext", "TermStructure",
+        date_reorg_data_df = pd.DataFrame(date_reorg_data_dic).T.sort_index(ascending=False)
+        date_reorg_data_df['instrument_type'] = instrument_type
+        date_reorg_data_df['adj_factor_main'] = date_reorg_data_df['adj_chg_main'].cumprod()
+        date_reorg_data_df['adj_factor_secondary'] = date_reorg_data_df['adj_chg_secondary'].cumprod()
+        col_list = ["trade_date", "Contract", "ContractNext", "Close", "CloseNext", "TermStructure",
                     "Volume", "VolumeNext", "OI", "OINext", "Open", "OpenNext", "High", "HighNext",
-                    "Low", "LowNext", "WarehouseWarrant", "WarehouseWarrantNext"]
+                    "Low", "LowNext", "WarehouseWarrant", "WarehouseWarrantNext",
+                    "adj_factor_main", "adj_factor_secondary", "instrument_type"]
         # 无复权价格
-        date_noadj_df = date_reorg_data_df[col_list].copy()
+        data_no_adj_df = date_reorg_data_df[col_list].copy()
+
         # 前复权价格
-        date_reorg_data_df['adj_factor_main'] = date_reorg_data_df.sort_index(ascending=False)['adj_chg_main'].cumprod()
         date_reorg_data_df['Open'] = date_reorg_data_df['Open'] * date_reorg_data_df['adj_factor_main']
         date_reorg_data_df['High'] = date_reorg_data_df['High'] * date_reorg_data_df['adj_factor_main']
         date_reorg_data_df['Low'] = date_reorg_data_df['Low'] * date_reorg_data_df['adj_factor_main']
         date_reorg_data_df['Close'] = date_reorg_data_df['Close'] * date_reorg_data_df['adj_factor_main']
-        date_reorg_data_df['adj_factor_secondary'] = date_reorg_data_df.sort_index(ascending=False)[
-            'adj_chg_secondary'].cumprod()
         date_reorg_data_df['OpenNext'] = date_reorg_data_df['OpenNext'] * date_reorg_data_df['adj_factor_secondary']
         date_reorg_data_df['HighNext'] = date_reorg_data_df['HighNext'] * date_reorg_data_df['adj_factor_secondary']
         date_reorg_data_df['LowNext'] = date_reorg_data_df['LowNext'] * date_reorg_data_df['adj_factor_secondary']
         date_reorg_data_df['CloseNext'] = date_reorg_data_df['CloseNext'] * date_reorg_data_df['adj_factor_secondary']
-        col_list = ["Date", "Contract", "ContractNext", "Close", "CloseNext", "TermStructure",
-                    "Volume", "VolumeNext", "OI", "OINext", "Open", "OpenNext", "High", "HighNext",
-                    "Low", "LowNext", "WarehouseWarrant", "WarehouseWarrantNext",
-                    "adj_factor_main", "adj_factor_secondary"]
-        date_adj_df = date_reorg_data_df[col_list].copy()
+
+        data_adj_df = date_reorg_data_df[col_list].copy()
 
         # date_adj_df.reset_index(inplace=True)
-        date_adj_df.rename(columns={
-            "Date": "trade_date"
-        }, inplace=True)
-        # 为了解决 AttributeError: 'numpy.float64' object has no attribute 'translate' 错误，需要将数据类型转换成 float
-        date_adj_df["Close"] = date_adj_df["Close"].apply(str_2_float)
-        date_adj_df["CloseNext"] = date_adj_df["CloseNext"].apply(str_2_float)
-        date_adj_df["TermStructure"] = date_adj_df["TermStructure"].apply(str_2_float)
-        date_adj_df["Volume"] = date_adj_df["Volume"].apply(str_2_float)
-        date_adj_df["VolumeNext"] = date_adj_df["VolumeNext"].apply(str_2_float)
-        date_adj_df["OI"] = date_adj_df["OI"].apply(str_2_float)
-        date_adj_df["OINext"] = date_adj_df["OINext"].apply(str_2_float)
-        date_adj_df["Open"] = date_adj_df["Open"].apply(str_2_float)
-        date_adj_df["OpenNext"] = date_adj_df["OpenNext"].apply(str_2_float)
-        date_adj_df["High"] = date_adj_df["High"].apply(str_2_float)
-        date_adj_df["HighNext"] = date_adj_df["HighNext"].apply(str_2_float)
-        date_adj_df["Low"] = date_adj_df["Low"].apply(str_2_float)
-        date_adj_df["LowNext"] = date_adj_df["LowNext"].apply(str_2_float)
-        date_adj_df["WarehouseWarrant"] = date_adj_df["WarehouseWarrant"].apply(str_2_float)
-        date_adj_df["WarehouseWarrantNext"] = date_adj_df["WarehouseWarrantNext"].apply(str_2_float)
-        date_adj_df["adj_factor_main"] = date_adj_df["adj_factor_main"].apply(str_2_float)
-        date_adj_df["adj_factor_secondary"] = date_adj_df["adj_factor_secondary"].apply(str_2_float)
-        date_adj_df['instrument_type'] = instrument_type
+        # data_adj_df.rename(columns={
+        #     "Date": "trade_date"
+        # }, inplace=True)
 
         if update_table:
-            table_name = 'reorg_md_daily'
-            # 清理历史记录
-            with with_db_session(engine_md) as session:
-                sql_str = """SELECT table_name FROM information_schema.TABLES 
-                    WHERE table_name = :table_name and TABLE_SCHEMA=(select database())"""
-                is_existed = session.execute(sql_str, params={"table_name": table_name}).fetchone()
-                if is_existed is not None:
-                    session.execute("delete from %s where instrument_type = :instrument_type" % table_name,
-                                    params={"instrument_type": instrument_type})
-                    logger.debug("删除%s 中的 %s 历史数据", table_name, instrument_type)
-            # 插入数据库
-            pd.DataFrame.to_sql(date_adj_df, table_name, engine_md, if_exists='append', index=False, dtype={
-                'trade_date': Date,
-                'Contract': String(20),
-                'ContractNext': String(20),
-                'Close': DOUBLE,
-                'CloseNext': DOUBLE,
-                'TermStructure': DOUBLE,
-                'Volume': DOUBLE,
-                'VolumeNext': DOUBLE,
-                'OI': DOUBLE,
-                'OINext': DOUBLE,
-                'Open': DOUBLE,
-                'OpenNext': DOUBLE,
-                'High': DOUBLE,
-                'HighNext': DOUBLE,
-                'Low': DOUBLE,
-                'LowNext': DOUBLE,
-                'WarehouseWarrant': DOUBLE,
-                'WarehouseWarrantNext': DOUBLE,
-                'adj_factor_main': DOUBLE,
-                'adj_factor_secondary': DOUBLE,
-                'instrument_type': String(20),
-            })
+            table_name = 'wind_future_continuous_adj'
+            update_df_2_db(instrument_type, table_name, data_adj_df)
+            table_name = 'wind_future_continuous_no_adj'
+            update_df_2_db(instrument_type, table_name, data_no_adj_df)
 
-    return date_noadj_df, date_adj_df
+    return data_no_adj_df, data_adj_df
 
 
-def export_2_csv(instrument_type_list, period="daily"):
+def handle_data(instrument_type_list, period="daily", update_table=True, export_2_csv=False):
     """将各个品种的，指定周期行情数据复权输出"""
     folder_path = os.path.join(os.path.abspath('.'), 'output', 'commodity_%s' % period)
     if not os.path.exists(folder_path):
@@ -324,14 +342,14 @@ def export_2_csv(instrument_type_list, period="daily"):
     for instrument_type in instrument_type_list:
         logger.info("开始导出 %s 相关数据", instrument_type)
         if period == "daily":
-            date_noadj_df, date_adj_df = data_reorg_daily(instrument_type, update_table=False)
+            date_noadj_df, date_adj_df = data_reorg_daily(instrument_type, update_table=update_table)
         # elif period == "half_day":
         #     date_noadj_df, date_adj_df = data_reorg_half_daily(instrument_type)
         # elif period in (PeriodType.Min1, PeriodType.Min5, PeriodType.Min15):
         #     date_noadj_df, date_adj_df = data_reorg_min_n(instrument_type, period)
         else:
             raise ValueError("%s 不支持" % period)
-        if date_noadj_df is not None:
+        if export_2_csv and date_noadj_df is not None:
             file_name = "%s ConInfoFull.csv" % instrument_type
             file_path = os.path.join(folder_path, file_name)
             logger.info(file_path)
@@ -343,16 +361,25 @@ def export_2_csv(instrument_type_list, period="daily"):
             logger.info(file_path)
 
 
+def wind_future_continuous_md():
+    """将期货合约数据合并成为连续合约数据，并保存数据库"""
+    instrument_type_list = get_all_instrument_type()
+    handle_data(instrument_type_list, period="daily", update_table=True, export_2_csv=False)
+
+
 if __name__ == "__main__":
+    # 将期货合约数据合并成为连续合约数据，并保存数据库
+    wind_future_continuous_md()
+
     # instrument_type_list = ["RU", "AG", "AU", "RB", "HC", "J", "JM", "I", "CU",
     #                         "AL", "ZN", "PB", "NI", "SN",
     #                         "SR", "CF"]
     # instrument_type_list = ["JM"]
-    instrument_type_list = get_all_instrument_type()
-    export_2_csv(instrument_type_list, period="daily")
-    # export_2_csv(instrument_type_list, period="half_day")
-    # export_2_csv(instrument_type_list, period=PeriodType.Min15)
-    # export_2_csv(instrument_type_list, period=PeriodType.Min5)
+    # instrument_type_list = get_all_instrument_type()
+    # handle_data(instrument_type_list, period="daily")
+    # handle_data(instrument_type_list, period="half_day")
+    # handle_data(instrument_type_list, period=PeriodType.Min15)
+    # handle_data(instrument_type_list, period=PeriodType.Min5)
 
     # 单独生成某合约历史数据
     # instrument_type = "RU"
