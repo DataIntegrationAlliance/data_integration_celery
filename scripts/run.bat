0bde716c83df06444187542e5a941ece77853092
mmmaaaggg <mmmaaaggg@163.com>
2019-01-21 15:28:25 +0800

    enhance run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,8 +1,10 @@
 echo off
+cd D:\WSPych\data_integration_celery
 :Begin
-choice /C:WBC /M:"run: Worker[W] Beat[B] Cancel[C]."
+choice /C:WBSC /M:"run: Worker[W] Beat[B] SingleWorker[S] Cancel[C]."
 echo "input is :" %errorlevel%
-if errorlevel 3 goto End
+if errorlevel 4 goto End
+if errorlevel 3 goto SingleWorker
 if errorlevel 2 goto Beat
 if errorlevel 1 goto Worker
 goto Begin
@@ -15,6 +17,11 @@ goto End
 celery beat -A tasks
 goto End
 
+:SingleWorker
+.\venv\Scripts\Python.exe -m tasks.tushare.__init__
+goto End
+
 :End
+
 echo on
 echo good bye
