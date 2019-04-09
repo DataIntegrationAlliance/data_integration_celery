fa0a82a4dd353f211d4ba3d3cf9ef6ee06d45f64
mmmaaaggg <mmmaaaggg@163.com>
2019-04-09 10:43:02 +0800

    code refine

--- tasks/tushare/tushare_stock_daily/daily_basic.py
+++ tasks/tushare/tushare_stock_daily/daily_basic.py
@@ -4,18 +4,15 @@ Created on 2018/8/13
 @desc    : 2018-08-3
 contact author:ybychem@gmail.com
 """
-import pandas as pd
 import logging
-from tasks.backend.orm import build_primary_key
 from datetime import date, datetime, timedelta
 from ibats_utils.mess import try_2_date, STR_FORMAT_DATE, datetime_2_str, split_chunk, try_n_times
 from tasks import app
 from sqlalchemy.types import String, Date, Integer
 from sqlalchemy.dialects.mysql import DOUBLE
 from tasks.backend import engine_md
-from tasks.merge.code_mapping import update_from_info_table
-from ibats_utils.db import with_db_session, add_col_2_table, alter_table_2_myisam, \
-    bunch_insert_on_duplicate_update
+from ibats_utils.db import with_db_session
+from tasks.backend import bunch_insert
 from tasks.tushare.ts_pro_api import pro
 
 DEBUG = False
@@ -65,7 +62,8 @@ def import_tushare_daily_basic(chain_param=None):
     logging.info("更新 %s 开始", table_name)
 
     has_table = engine_md.has_table(table_name)
-    # 下面一定要注意引用表的来源，否则可能是串，提取混乱！！！比如本表是tushare_daily_basic，所以引用的也是这个，如果引用错误，就全部乱了l
+    # 下面一定要注意引用表的来源，否则可能是串，提取混乱！！！
+    # 比如本表是 tushare_daily_basic，所以引用的也是这个，如果引用错误，就全部乱了
     if has_table:
         sql_str = """
                select cal_date            
@@ -87,28 +85,22 @@ def import_tushare_daily_basic(chain_param=None):
     with with_db_session(engine_md) as session:
         # 获取交易日数据
         table = session.execute(sql_str)
-        trddate = list(row[0] for row in table.fetchall())
+        trade_date_list = list(row[0] for row in table.fetchall())
 
     try:
-        for i in range(len(trddate)):
-            trade_date = datetime_2_str(trddate[i], STR_FORMAT_DATE_TS)
+        for_count = len(trade_date_list)
+        for num, trade_date in enumerate(trade_date_list, start=1):
+            trade_date = datetime_2_str(trade_date, STR_FORMAT_DATE_TS)
             data_df = invoke_daily_basic(ts_code='', trade_date=trade_date)
             if data_df is not None and data_df.shape[0] > 0:
-                data_count = bunch_insert_on_duplicate_update(data_df, table_name, engine_md, DTYPE_TUSHARE_STOCK_DAILY_BASIC)
-                logging.info("%s更新 %s 结束 %d 条信息被更新", trade_date, table_name, data_count)
+                data_count = bunch_insert(
+                    data_df, table_name=table_name, dtype=DTYPE_TUSHARE_STOCK_DAILY_BASIC,
+                    primary_keys=["ts_code", "trade_date"])
+                logging.info("%d/%d) %s 更新 %s 结束 %d 条信息被更新", num, for_count, trade_date, table_name, data_count)
             else:
-                logging.info("无数据信息可被更新")
-    finally:
-        if not has_table and engine_md.has_table(table_name):
-            alter_table_2_myisam(engine_md, [table_name])
-            # build_primary_key([table_name])
-            create_pk_str = """ALTER TABLE {table_name}
-                CHANGE COLUMN `ts_code` `ts_code` VARCHAR(20) NOT NULL FIRST,
-                CHANGE COLUMN `trade_date` `trade_date` DATE NOT NULL AFTER `ts_code`,
-                ADD PRIMARY KEY (`ts_code`, `trade_date`)""".format(table_name=table_name)
-            with with_db_session(engine_md) as session:
-                session.execute(create_pk_str)
-            logger.info('%s 表 `ts_code`, `trade_date` 主键设置完成', table_name)
+                logging.info("%d/%d) %s 无数据信息可被更新",  num, for_count, trade_date)
+    except:
+        logger.exception("更新 %s 表异常", table_name)
 
 
 if __name__ == "__main__":
