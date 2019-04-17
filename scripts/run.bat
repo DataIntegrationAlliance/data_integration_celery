2d571251bbe6b19abf499e30eb0c07ecbc41ba32
mmmaaaggg <mmmaaaggg@163.com>
2019-04-17 10:57:02 +0800

    add first job

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,9 +2,10 @@ echo off
 
 cd D:\WSPych\data_integration_celery
 :Begin
-choice /C:AWBCTJ /M:"run: Active Env[A] Worker[W] Beat[B] Cancel[C] Tushare Local[T] Join Quant Local[J]"
+choice /C:AWBCTJ /M:"run: Active Env[A] Worker[W] Beat[B] Cancel[C] Tushare Local[T] Tushare Local First[F] Join Quant Local[J]"
 echo "input is :" %errorlevel%
-if errorlevel 6 goto JoinQuantLocal
+if errorlevel 7 goto JoinQuantLocal
+if errorlevel 6 goto TushareLocalFirst
 if errorlevel 5 goto TushareLocal
 if errorlevel 4 goto End
 if errorlevel 3 goto Beat
@@ -24,8 +25,12 @@ goto End
 celery beat -A tasks
 goto End
 
+:TushareLocalFirst
+.\venv\Scripts\Python.exe -m tasks.tushare.app_tasks first
+goto End
+
 :TushareLocal
-.\venv\Scripts\Python.exe -m tasks.tushare.__init__
+.\venv\Scripts\Python.exe -m tasks.tushare.app_tasks
 goto End
 
 :JoinQuantLocal
