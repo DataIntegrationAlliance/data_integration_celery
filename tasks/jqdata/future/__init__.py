4921539ff9785dc650016522041d12cb83e7716e
mmmaaaggg <mmmaaaggg@163.com>
2019-04-19 14:56:00 +0800

    add future module

---
+++ tasks/jqdata/future/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 19-4-19 下午2:52
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+if __name__ == "__main__":
+    pass
