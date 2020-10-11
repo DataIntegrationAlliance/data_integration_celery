39c9ca94345c0412294b4316c89b70fc7b59107a
mmmaaaggg <mmmaaaggg@163.com>
2020-10-11 14:45:58 +0800

    feat: add rqdatac

---
+++ tasks/rqdatac/future.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+"""
+@author  : MG
+@Time    : 2020/10/11 下午2:08
+@File    : future.py
+@contact : mmmaaaggg@163.com
+@desc    : 导入 rqdatac 期货行情数据
+"""
+
+
+if __name__ == "__main__":
+    pass
