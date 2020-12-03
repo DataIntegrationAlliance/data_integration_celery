7263767b641c8a8ff649da4eb4a3ab992f89b35c
maguo <mmmaaaggg@163.com>
2020-12-03 16:09:35 +0800

    add add invoker.py

---
+++ tasks/mysteel/__init__.py
@@ -0,0 +1,10 @@
+"""
+@author  : MG
+@Time    : 2020/12/3 16:05
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ tasks/mysteel/invoker.py
@@ -0,0 +1,10 @@
+"""
+@author  : MG
+@Time    : 2020/12/3 16:06
+@File    : invoker.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于我的钢铁 invoker
+"""
+
+if __name__ == "__main__":
+    pass
