b8a7d62fcc235fe11f883bc2011c867380ba6ef6
maguo <mmmaaaggg@163.com>
2021-01-08 09:23:33 +0800

    feat: update by instrument_types

--- tasks/wind/future_mid_day_task.py
+++ tasks/wind/future_mid_day_task.py
@@ -5,13 +5,18 @@
 @contact : mmmaaaggg@163.com
 @desc    : 用于
 """
-from tasks.wind.future import import_future_min, min_to_vnpy_increment
+from tasks.wind.future import import_future_min, min_to_vnpy_increment, get_main_secondary_contract_by_instrument_types
 
 
 def run_mid_day_task():
-    import_future_min(
-        # wind_code_set=('RB2101.SHF', 'RB2105.SHF', 'RB2110.SHF', 'HC2101.SHF', 'HC2105.SHF', 'HC2110.SHF'),
-    )
+    wind_code_set = None
+    # wind_code_set = (
+    #     'RB2105.SHF', 'RB2110.SHF',
+    #     'HC2105.SHF', 'HC2110.SHF',
+    # )
+    instrument_types = ['rb', 'hc', 'i', 'j', 'jm', 'jd', 'ap', 'a', 'p', 'm', 'y']
+    wind_code_set = get_main_secondary_contract_by_instrument_types(instrument_types=instrument_types)
+    import_future_min(wind_code_set=wind_code_set, )
     min_to_vnpy_increment(
         # instrument_types=['rb', 'i', 'hc']
     )
