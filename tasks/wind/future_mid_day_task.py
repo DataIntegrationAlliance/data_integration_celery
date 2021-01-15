e655dbf03c4d1b581d22bff7f6d8451a1378ceca
maguo <mmmaaaggg@163.com>
2021-01-15 16:57:41 +0800

    refactor: update instrument_types = ['rb', 'hc', 'i', 'j', 'jm', 'jd', 'ap', 'a', 'p', 'm', 'y', 'b']

--- tasks/wind/future_mid_day_task.py
+++ tasks/wind/future_mid_day_task.py
@@ -14,8 +14,8 @@ def run_mid_day_task():
     #     'RB2105.SHF', 'RB2110.SHF',
     #     'HC2105.SHF', 'HC2110.SHF',
     # )
-    instrument_types = ['rb', 'hc', 'i', 'j', 'jm', 'jd', 'ap', 'a', 'p', 'm', 'y']
-    wind_code_set = get_main_secondary_contract_by_instrument_types(instrument_types=instrument_types)
+    instrument_types = ['rb', 'hc', 'i', 'j', 'jm', 'jd', 'ap', 'a', 'p', 'm', 'y', 'b']
+    # wind_code_set = get_main_secondary_contract_by_instrument_types(instrument_types=instrument_types)
     import_future_min(wind_code_set=wind_code_set, )
     min_to_vnpy_increment(
         # instrument_types=['rb', 'i', 'hc']
