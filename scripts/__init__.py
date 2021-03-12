5227d5aefdc8ef0652dc388fad2c565ce78c08ee
maguo <mmmaaaggg@163.com>
2021-03-13 05:07:00 +0800

    feat: add whole_transfer_2_vnpy_dbbardata

---
+++ scripts/__init__.py
@@ -0,0 +1,10 @@
+"""
+@author  : MG
+@Time    : 2021/3/13 4:56
+@File    : __init__.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ scripts/whole_transfer_2_vnpy_dbbardata.py
@@ -0,0 +1,19 @@
+"""
+@author  : MG
+@Time    : 2021/3/13 4:57
+@File    : whole_transfer_2_vnpy_dbbardata.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于全量的将历史期货分钟级数据导入到vnpy数据库中
+使用 rqdata 以及 wind 数据进行结合
+"""
+from tasks.rqdatac.future import min_to_vnpy
+from tasks.wind.future import min_to_vnpy_whole
+
+
+def transfer():
+    min_to_vnpy()
+    min_to_vnpy_whole()
+
+
+if __name__ == "__main__":
+    transfer()
